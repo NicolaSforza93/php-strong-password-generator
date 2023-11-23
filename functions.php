a0e7a7e02ec09d6dd2941686a474fbbbab1750c5
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-11-23 18:34:45 +0100

    added Milestone 2

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+$length = $_GET['length_string'];
+$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!?&.:;+_-%';
+
+
+function passwordGenerator($arg1)
+{
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!?&.:;+_-%';
+    $length = $_GET['length_string'];
+    $Password = '';
+    // var_dump($characters, $length);
+    for ($i = 0; $i < $length; $i++) {
+        $randChars = mt_rand(0, strlen($characters) - 1);
+        $Password .= $characters[$randChars];
+    }
+    return $Password;
+}
+
+// var_dump(passwordGenerator($length));
--- index.php
+++ index.php
@@ -4,39 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>PHP Strong Password Generator</title>
 </head>
 
 <body>
     <?php
-    $length = $_GET['length_string'];
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!?&.:;+_-%';
 
-
-    function passwordGenerator($arg1)
-    {
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!?&.:;+_-%';
-        $length = $_GET['length_string'];
-        $Password = '';
-        var_dump($characters, $length);
-        for ($i = 0; $i < $length; $i++) {
-            $randChars = mt_rand(0, strlen($characters) - 1);
-            $Password .= $characters[$randChars];
-        }
-        return $Password;
-    }
-
-    var_dump(passwordGenerator($length));
-
+    require __DIR__ . '/functions.php';
 
     ?>
+    <div class="container p-4">
+        <form action="" method="GET" class="d-flex align-items-center gap-3 mb-3">
+            <div class="d-flex align-items-center gap-2">
+                <label for="length_string"><strong>Inserisci un numero</strong></label>
+                <input type="number" name="length_string" id="length_string">
+            </div>
+            <button type="submit" class="btn btn-primary btn-sm">Genera</button>
+        </form>
 
-    <form action="" method="GET">
-        <label for="length_string">Inserisci un numero</label>
-        <input type="number" name="length_string" id="length_string">
-        <button type="submit">Invia</button>
-    </form>
-
+        <p><strong>Password generata</strong>: <strong class="text-danger"><?php echo passwordGenerator($length) ?></strong></p>
+    </div>
 </body>
 
 </html>
