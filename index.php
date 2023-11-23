ed94e605690f6afaf2dfae00936b77d4e1abd4e7
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-11-23 18:03:01 +0100

    added Milestone 1

--- index.php
+++ index.php
@@ -8,6 +8,34 @@
 </head>
 
 <body>
+    <?php
+    $length = $_GET['length_string'];
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!?&.:;+_-%';
+
+
+    function passwordGenerator($arg1)
+    {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!?&.:;+_-%';
+        $length = $_GET['length_string'];
+        $Password = '';
+        var_dump($characters, $length);
+        for ($i = 0; $i < $length; $i++) {
+            $randChars = mt_rand(0, strlen($characters) - 1);
+            $Password .= $characters[$randChars];
+        }
+        return $Password;
+    }
+
+    var_dump(passwordGenerator($length));
+
+
+    ?>
+
+    <form action="" method="GET">
+        <label for="length_string">Inserisci un numero</label>
+        <input type="number" name="length_string" id="length_string">
+        <button type="submit">Invia</button>
+    </form>
 
 </body>
 
